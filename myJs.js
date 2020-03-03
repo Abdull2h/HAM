38486146735b7f6aa2665d9b20f80a223c08179b
Abdull2h <aboode.797@gmail.com>
2020-03-03 10:10:38 +0300

    added dates to cards.

--- myJs.js
+++ myJs.js
@@ -2,32 +2,39 @@ var cards = {
     card1: {
         pic: "https://fakeimg.pl/700x250",
         title: "This is our first title",
-        desc: "this is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        desc: "this is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        date: "1 / March"
     },
     card2: {
         pic: "https://fakeimg.pl/700x250",
         title: "Second Title",
-        desc: "Second description"
+        desc: "Second description",
+        date: "28 / February"
+
     },
     card3: {
         pic: "https://fakeimg.pl/700x250",
         title: "Third",
-        desc: "this is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        desc: "this is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        date: "27 / February"
     },
     card4: {
         pic: "https://fakeimg.pl/700x250",
         title: "Fourth Card",
-        desc: "Fourth description"
+        desc: "Fourth description",
+        date: "22 / February"
     },
     card5: {
         pic: "https://fakeimg.pl/700x250",
         title: "Game news network",
-        desc: "our news"
+        desc: "our news",
+        date: "21 / February"
     },
     card6: {
         pic: "https://fakeimg.pl/700x250",
         title: "out of ideas",
-        desc: "sixth description"
+        desc: "sixth description",
+        date: "20 / February"
     }
 
 }
@@ -40,7 +47,7 @@ function cardPrint(container) {
         card += '<div class="card-body">'
         card += '<h5 class="card-title text-info text-center">' + cards[key].title + '</h5>'
         card += '<p class="card-text text-justify">' + cards[key].desc + '</p>'
-        card += '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>'
+        card += '<p class="card-text"><small class="text-muted">' + cards[key].date + '</small></p></div></div>'
         document.getElementById(container).innerHTML += card;
         console.log("loops")
     }
