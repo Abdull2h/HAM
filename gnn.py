c14c90866c00e6faab427f20692ccb1c9094d9e0
mohdaim996 <moh.daim996@gmail.com>
2020-03-15 01:28:12 +0300

    Sessions added

--- gnn.py
+++ gnn.py
@@ -1,14 +1,16 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session,redirect
 from data import storeData,readData
 
 
 app = Flask(__name__)
 app.secret_key='c757593904c759f0b8b463ee0165bced'
 
+   
 @app.route("/")
 @app.route("/index.html")
 @app.route("/index")
 def home():
+   
     return render_template('index.html', title = 'Home Page')
     
 @app.route("/about_us.html")
@@ -24,6 +26,8 @@ def contact_us():
 @app.route("/regestration.html")
 @app.route("/regestration")
 def register():
+    if session.get('logged_in') == True:
+        return redirect('/index.html')
     return render_template('regestration.html', title = 'Register')
 
 @app.route('/handle_data', methods=['POST', 'GET'])
@@ -51,6 +55,8 @@ def handle_data():
 @app.route("/login.html")
 @app.route("/login")
 def login():
+    if session.get('logged_in') == True:
+        return redirect('/index.html')
     return render_template('login.html', title = 'Login')
 
 @app.route("/users.html")
@@ -79,4 +85,33 @@ def adventure():
     post=getpost['adventure']
     return render_template('adventure.html', title = 'Adventure Games',post=post)
 
+@app.route('/set', methods=['POST', 'GET'])
+def set():
+    us = request.form['name']
+    pw = request.form['password']
+    data = readData('data.json')
+    for name in data.keys():
+        if us == name:
+            if pw == data[name]['pass']:
+                session['current_user'] = us
+                session['logged_in']=True
+                return redirect('/index.html')
+            else:
+                return render_template('login.html', us = 'wrong pass')
+        
+    return render_template('login.html', us='wrong username')
+
+    
+
+@app.route('/get/')
+def get():
+    if session.get('logged_in') == True:
+        return 'You are logged in'
+    return 'You are not logged in'
+
+@app.route('/pop/')
+def pop():
+    session.pop('current_user')
+    session.pop('logged_in')
+    return f"cuurent_user is removed"
 app.run(debug=True)
